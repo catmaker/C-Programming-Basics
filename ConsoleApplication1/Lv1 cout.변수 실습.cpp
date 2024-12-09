13601e1d3ae3763ed16522829ec5674ecaec41e5
rzt0317 <rzt0357@gmail.com>
2024-12-09 18:26:40 +0900

    1Lv cout, 변수

---
+++ ConsoleApplication1/Lv1 cout.변수 실습.cpp
@@ -0,0 +1,119 @@
+#include <iostream>
+using namespace std;
+int main() {
+	cout << "Hello world" << endl;
+
+	cout << "@@@@" << endl;
+
+	cout << "main()" << endl;
+
+	cout << "std::cout << \"@@@@\"" << endl;
+
+	cout << "123###456" << endl;
+
+	cout << "CODING" << endl;
+
+	cout << "##   ##" << endl;
+
+	cout << "std::cout \"CODING\"" << endl;
+
+	cout << "std::cout << std::endl;" << endl;
+
+	cout << "std::cout << \"##   ##\"" << endl;
+
+	cout << "5 4 3 2 1 GO!" << endl;
+
+	int a = 5;
+
+	cout << a << endl;
+
+	int t = 5;
+
+	cout << "t에서 1씩 증가 =5 6 7" << endl;
+	cout << "t에서 2씩 감소 =5 3 1" << endl;
+
+	int b = -1;
+	int c = 2;
+
+	cout << "a, b, c 값은 각각 a는 5, b는 -1, c는 2입니다" << endl;
+
+	a = 7;
+	b = 2;
+	int sum = a + b;
+	int minus = a - b;
+	cout << "a+b=" << sum << endl;
+	cout << "a-b=" << minus << endl;
+
+	int k = 8;
+	int g = 4;
+
+	cout << "8*4=" << k * g << endl;
+	cout << "8/4=" << k / g << endl;
+
+	a = 3;
+	b = 5;
+	c = 3;
+	int d = 4;
+
+	cout << ((a + b) * c) / d << endl;
+
+	a = 8;
+	cout << a * a * a * a * a << endl;
+
+	
+	cout << a+(a/2) << endl;
+
+	cout << 1234 << endl << " 567" << endl << "  89" << endl << "   0" << endl;
+
+	cout << "158% 입니다" << endl;
+
+	int q = 1;
+	int w = 2;
+	int e = 3;
+
+	cout << q << "," << q << "," << q << endl;
+	cout << w << "," << w << "," << w << endl;
+	cout << e << "," << e << "," << e << endl;
+	
+	a = 10;
+	cout << "a의 값은 10입니다" << endl;
+
+	int bbq = 5;
+
+	cout << "bbq의 값은" << bbq << " 입니다" << endl;
+
+	a = 40;
+	b = 60;
+	c = 10;
+	g = a + c;
+	int h = b - c;
+
+	cout << g << endl << h << endl;
+
+	a = 10;
+	b = 3;
+
+	cout << a << "*" << b << " = " << a*b << endl;
+	cout << a << "/" << b << " = " << a / b << endl;
+
+	a = 3;
+	b = 2;
+	c = 6;
+	d = 2;
+
+	cout << (a + b) * (c / d) << endl;
+
+	a = 3;
+	b = 3;
+	c = 8;
+	d = 3;
+
+	cout << (a * b) + (c * d) << endl;
+
+
+
+
+
+
+	return 0;
+}
